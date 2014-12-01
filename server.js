ddbc28729e967affa696420a89d01b3c3a121bad
Kev Hunter <MrKevHunter@gmail.com>
2014-12-01 21:18:48 +0000

    can now query tp

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express')
 var app = express()
+var rest = require('restler');
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+	var options = {
+		'username':'',
+		'password':'',
+		'headers': {'Content-type': 'application/json', 'Accept': 'application/json'}
+	}
+  rest.get('https://neworbit.tpondemand.com/api/v1/Context/',options).on('complete', function(data) {
+	res.json(data);
+});
 })
+
+
 
 app.listen(56012)
